fa8defecda22a64c95a9f3da23db49c052c4e7a1
rawadsalman <rwadsalman81@gmail.com>
2025-12-06 23:17:08 +0100

    Fix import and usage of WhatsApp barcode image

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Services from './Services';
 import { FaPhoneAlt, FaWhatsapp } from 'react-icons/fa';
 import { motion } from "framer-motion";
-import Whatsapp-barcode-4 from '../Whatsapp-barcode-4.jpg';
+import WhatsappBarcode4 from '../Whatsapp-barcode-4.jpg';
 
 
 import '../styles/home.css'; 
@@ -28,7 +28,7 @@ function Home() {
                <FaWhatsapp style={{ fontSize: "40px", color: "#25D366" }}/>
                </motion.div>
 			<img
-              src={Whatsapp-barcode-4}
+              src={WhatsappBarcode4}
               alt="WhatsApp Barcode"
               style={{ width: "100px", marginLeft: "10px" }}
             />
